bfcac79d4d7ee85fdc4d4326915b0a752bf16eb1
Eirik Backer <Eirik.Backer@nrk.no>
2017-10-17 14:40:21 +0200

    Add webpack

---
+++ all.js
@@ -0,0 +1,3 @@
+module.exports = {
+  details: require('./details')
+}
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const config = {
+  entry: {
+    'core-components': './all.js',
+    'core-components.min': './all.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].js',
+    library: 'core-components',
+    libraryTarget: 'umd'
+  },
+  context: __dirname,
+  devServer: {
+    contentBase: [__dirname]
+  },
+  module: {
+    rules: [{
+      test: /\.js?$/,
+      exclude: [/node_modules/],
+      use: [{
+        loader: 'babel-loader',
+        options: { presets: ['env'] }
+      }]
+    }]
+  },
+  plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      include: /(\.min)\.js$/,
+      sourceMap: true
+    })
+  ]
+}
+
+module.exports = config
