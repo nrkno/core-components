4ec444f02f21d74e4c75b7b9af44b52034e023f4
Eirik Backer <Eirik.Backer@nrk.no>
2017-12-10 19:27:29 +0100

    Upgrade dependencies

--- dist/core-components.jsx.js
+++ dist/core-components.jsx.js
@@ -7,7 +7,7 @@
 		exports["core-components"] = factory();
 	else
 		root["core-components"] = factory();
-})(this, function() {
+})(typeof self !== 'undefined' ? self : this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
