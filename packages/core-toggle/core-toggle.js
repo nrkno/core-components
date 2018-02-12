2bc686398ef3b7467a0648a88e7dbf29f3f12e2b
Eirik Backer <Eirik.Backer@nrk.no>
2018-02-12 09:21:39 +0100

    Update Uglify and minify all files

--- packages/core-toggle/core-toggle.js
+++ packages/core-toggle/core-toggle.js
@@ -4,20 +4,29 @@ import {addElements, addEvent, CustomEvent} from '../utils'
 const KEY = `${name}-${version}`                    // Unique id of component
 const ATTR = 'aria-expanded'
 
-export function toggle (elems, open = null) {       // Toggle component
-  return addElements(KEY, elems).map((el) => {
-    const isExpand = el.getAttribute(ATTR) === 'true'
-    const doExpand = open === null ? isExpand : Boolean(open)
-    const canSetup = isExpand === doExpand || el.dispatchEvent(new CustomEvent('toggle', {
+/**
+* toggle
+* @param {String|NodeList|Array|Element} elements A CSS selector string, nodeList, element array, or single element
+* @param {Boolean} [open] Optional open argument. True will open, false will close
+* @return {Array} Array of elements
+*/
+export function toggle (elements, open) {
+  const action = typeof open
+
+  return addElements(KEY, elements).map((el, index) => {
+    const prevState = el.getAttribute(ATTR) === 'true'
+    const nextState = action === 'undefined' ? prevState : Boolean(action === 'function' ? open(el, index) : open)
+    const canUpdate = prevState === nextState || el.dispatchEvent(new CustomEvent('toggle', {
       bubbles: true,
-      cancelable: true
+      cancelable: true,
+      detail: nextState
     }))
 
-    if (canSetup) el.setAttribute(ATTR, doExpand)
+    if (canUpdate) el.setAttribute(ATTR, Boolean(open))
     return el
   })
 }
 
 addEvent(KEY, 'click', (el) => {
-  toggle(el, el.getAttribute(ATTR) === 'false')
+  toggle(el, el.getAttribute(ATTR) !== 'true')
 })
--- rollup.config.js
+++ rollup.config.js
@@ -4,47 +4,43 @@ const json = require('rollup-plugin-json')
 const resolve = require('rollup-plugin-node-resolve')
 const serve = require('rollup-plugin-serve')
 const uglify = require('rollup-plugin-uglify')
-const {pkgs} = require('./bin/index.js')                   // Find all packages
+const {pkgs} = require('./bin/index.js')                      // Find all packages
 
-const isBuild = !process.env.ROLLUP_WATCH
-const globals = {'react-dom': 'ReactDOM', react: 'React'} // Do not include react in out package
-const external = Object.keys(globals)                     // Do not include react in out package
+const paths = pkgs.concat('.')                                // Path to all packages and root bundle
+const globals = {'react-dom': 'ReactDOM', react: 'React'}     // Do not include react in out package
 const plugins = [
-  json(),                                                 // Enable treeshaking json imports
-  resolve({browser: true}),                               // Respect pkg.browser
-  commonjs({ignoreGlobal: true}),                         // Let dependencies use the word `global`
-  buble({objectAssign: 'assign'})                         // Polyfill Object.assign from utils.js
+  json(),                                                     // Enable treeshaking json imports
+  resolve({browser: true}),                                   // Respect pkg.browser
+  commonjs({ignoreGlobal: true}),                             // Let dependencies use the word `global`
+  buble({objectAssign: 'assign'}),                            // Polyfill Object.assign from utils.js
+  uglify()
 ]
 
-export default pkgs.concat('.').reduce((acc, path) => {   // Loop all packages and add root bundle
+export default paths.map((path, index) => {                   // Make config for all packages
   const pkg = require(`${path}/package.json`)
-  const src = pkg.main.replace('.cjs.js', '.js')          // Source files is always just .js
-  const config = [{
+  const src = pkg.main.replace('.cjs.js', '.js')              // Source files is always just .js
+  const watch = !index && process.env.ROLLUP_WATCH            // Only watch if first index
+  const config = {
     input: `${path}/${src}`,
     output: [
-      {file: `${path}/${pkg.main}`, format: 'cjs'},       // CommonJS (for Node)
-      {file: `${path}/${pkg.module}`, format: 'es'}       // ES module (for bundlers)
+      {file: `${path}/${pkg.main}`, format: 'cjs', globals},  // CommonJS (for Node)
+      {file: `${path}/${pkg.module}`, format: 'es', globals}  // ES module (for bundlers)
     ],
-    external,
-    globals,
+    watch: {exclude: '**.*.(min|js)'},
+    external: Object.keys(globals),
     plugins
-  }]
+  }
 
+  if (watch) config.plugins.push(serve('bundle'))
   if (pkg.browser) {
-    config.push({
-      input: `${path}/${src}`,
-      output: {
-        file: pkg.browser,                                  // UDM for browsers
-        format: 'umd',
-        name: 'coreComponents',
-        sourcemap: true
-      },
-      watch: { exclude: '**.*.(min|js)' },
-      plugins: plugins.concat(isBuild ? uglify() : serve('bundle')),
-      external,
+    config.output.push({
+      name: pkg.name.split('-').pop(),
+      file: `${path}/${pkg.browser}`,                         // UDM for browsers
+      format: 'umd',
+      sourcemap: true,
       globals
     })
   }
 
-  return acc.concat(config)
-}, [])
+  return config
+})
