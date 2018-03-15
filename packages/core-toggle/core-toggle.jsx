a61cece9b10cd644f4c053e8281d0ddb6eb73af9
William Almnes <william.almnes@nrk.no>
2018-03-15 11:23:07 +0100

    fix minor spacing issue

--- packages/core-toggle/core-toggle.jsx
+++ packages/core-toggle/core-toggle.jsx
@@ -8,7 +8,7 @@ function mountToggle (self) {
 }
 
 export default class Toggle extends React.Component {
-  componentDidMount () { mountToggle(this) }                     // Mount client side only to avoid rerender
+  componentDidMount () { mountToggle(this) }                    // Mount client side only to avoid rerender
   componentDidUpdate () { mountToggle(this) }                   // Must mount also on update in case content changes
   render () {
     return <div {...assign({}, this.props, {open: null, popup: null})}>
