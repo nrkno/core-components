6e9e0a073d30d933e27fb8b702281537ee44a282
Eirik Backer <eirik.backer@gmail.com>
2018-04-06 16:53:51 +0200

    Fix conflic where event bubling messed exectuion order for JSX events (#14)
    Fix conflict where event bubling messed exectuion order for JSX events

--- packages/core-toggle/core-toggle.jsx
+++ packages/core-toggle/core-toggle.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import coreToggle from '../core-toggle/core-toggle'
 import {exclude} from '../utils'
 
-const DEFAULTS = {open: null, popup: null}
+const DEFAULTS = {open: null, popup: null, onToggle: null} // Remove onToggle prop as this is also a native event
 
 export default class Toggle extends React.Component {
-  componentDidMount () { coreToggle(this.el.firstElementChild) } // Mount client side only to avoid rerender
+  componentDidMount () {
+    coreToggle(this.el.firstElementChild) // Mount client side only to avoid rerender
+    this.el.addEventListener('toggle', this.props.onToggle)
+  }
   componentDidUpdate () { coreToggle(this.el.firstElementChild) } // Must mount also on update in case content changes
+  componentWillUnmount () { this.el.removeEventListener('toggle', this.props.onToggle) }
   render () {
     return React.createElement('div', exclude(this.props, DEFAULTS, {ref: (el) => (this.el = el)}),
       React.Children.map(this.props.children, (child, adjacent) => adjacent
