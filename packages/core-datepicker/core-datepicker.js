fbb3fa0b86440acfa5eb9e1c686a5a643dda7ae8
Eirik Backer <Eirik.Backer@nrk.no>
2018-04-08 13:24:43 +0200

    Add datepicker.parse

--- packages/core-datepicker/core-datepicker.js
+++ packages/core-datepicker/core-datepicker.js
@@ -1,7 +1,7 @@
 import {name, version} from './package.json'
 import {queryAll, addEvent} from '../utils'
-import strtotime from './strtotime'
 
+const ATTR = `data-${name.split('/').pop()}` // Name without scope
 const UUID = `data-${name}-${version}`.replace(/\W+/g, '-') // Strip invalid attribute characters
 const KEYS = {ENTER: 13, ESC: 27, PAGEUP: 33, PAGEDOWN: 34, END: 35, HOME: 36, UP: 38, DOWN: 40}
 const CODE = Object.keys(KEYS).reduce((all, key) => (all[KEYS[key]] = key) && all, {})
@@ -10,15 +10,17 @@ const DAYS = ['man', 'tirs', 'ons', 'tors', 'fre', 'lør', 'søn']
 const MONTHS = ['januar', 'februar', 'mars', 'april', 'mai', 'juni', 'juli', 'august', 'september', 'oktober', 'november', 'desember']
 const RENDERS = {select: 'month', input: 'year', table: 'day'}
 
-export default function datepicker (buttons, options = {}) {
-  return queryAll(buttons).map((button) => {
-    const date = parseDate(button.value)
+export default function datepicker (buttons, date = {}) {
+  const options = date.constructor === Object ? date :  {date} // Date is object so check cunstructor
 
-    button.setAttribute(UUID, '')
-    button.value = date.toISOString() // Store date value
+  return queryAll(buttons).map((button) => {
+    const show = datepicker.parse(options.date || button.getAttribute(UUID) || button.value)
+
+    button.setAttribute(UUID, show.toJSON())
+    // button.value = show.toISOString() // Store date value
 
     queryAll(Object.keys(RENDERS).join(','), button.nextElementSibling).forEach((el) => {
-      datepicker[RENDERS[el.nodeName.toLowerCase()]](el, date, date)
+      datepicker[RENDERS[el.nodeName.toLowerCase()]](el, show, show)
     })
     return button
   })
@@ -39,7 +41,7 @@ datepicker.day = function (table, date, selected){
   const today = new Date()
   const year = date.getFullYear()
   const month = date.getMonth()
-  let day = getMonday(new Date(year, month, 1))
+  let day = datepicker.parse('yyyy-mm-01 mon', date) // Start on first monday of month
   let html = ''
 
   for (let i = 0; i < 6; i++) {
@@ -49,58 +51,41 @@ datepicker.day = function (table, date, selected){
       const isSelected = isSameDay(day, selected)
       const isToday = isSameDay(day, today)
 
-      html += `<td>
-        <button title="${isToday ? 'I dag' : ''}" aria-pressed="${isSelected}" style="${isDayInMonth ? '' : 'opacity:.3'}${isToday ? ';color:red' : ''}">
-          ${day.getDate()}
-        </button>
-      </td>`
+      html += `<td><button title="${isToday ? 'I dag' : ''}" aria-pressed="${isSelected}" style="${isDayInMonth ? '' : 'opacity:.3'}${isToday ? ';color:red' : ''}">`
+      html += `${day.getDate()}</button></td>`
       day.setDate(day.getDate() + 1)
     }
     html += '</tr>'
   }
 
-  table.innerHTML = `<caption>${MONTHS[month]}, ${year}</caption>
-  <thead><tr><th>${DAYS.join('</th><th>')}</th></tr></thead>
-  <tbody>${html}</tbody>`
+  table.innerHTML = `<caption>${MONTHS[month]}, ${year}</caption><thead><tr><th>${DAYS.join('</th><th>')}</th></tr></thead><tbody>${html}</tbody>`
 }
 
 const isSameYear = (d1, d2) => d1.getFullYear() === d2.getFullYear()
 const isSameMonth = (d1, d2) => isSameYear(d1, d2) && d1.getMonth() === d2.getMonth()
 const isSameDay = (d1, d2) => isSameYear(d1, d2) && isSameMonth(d1, d2) && d1.getDate() === d2.getDate()
 
-function parseDate (date) {
-  return date instanceof Date ? date : new Date(strtotime(date || 'now'))
-}
-
-// https://stackoverflow.com/questions/4156434/javascript-get-the-first-day-of-the-week-from-current-date#answer-4156562
-function getMonday(date) {
-  const mon = new Date(date)
-  const day = date.getDay() || 7 // Converting Sunday to 7
-  mon.setHours(day === 1 ? 0 : (day - 1) * -24)
-  return mon
-}
-
-function closest (element) {
-  for (let el = element; el; el = el.parentElement) {
-    const prev = el.previousElementSibling
-    if (prev && prev.hasAttribute(UUID)) return prev
+function closest (target) {
+  for (var currentTarget = target; target; target = target.parentElement) {
+    const prev = target.previousElementSibling ||target
+    const attr = target.getAttribute(ATTR) // Will fail when empty
+    if (attr) return {currentTarget: target, target: document.querySelector(attr)}
+    if (target.hasAttribute(UUID) || prev.hasAttribute(UUID)) break
   }
+  return {target, currentTarget}
 }
 
 addEvent(UUID, 'input', onChange)
-addEvent(UUID, 'change', onChange)
+addEvent(UUID, 'change', onChange) // IE/Edge does not trigger input on <select>
 addEvent(UUID, 'click', (event) => {
-  for (let el = event.target; el; el = el.parentElement) {
-    if (el.name === 'day') {
-      const element = closest(event.target)
-      const content = element.nextElementSibling
-      const table = content.querySelector('table')
-      const show = new Date(element.value)
-      const date = new Date(strtotime(el.value) * 1000)
-      console.log(show, date)
+  const {target, currentTarget} = closest(event.target)
+  console.log(closest(event.target))
 
-      datepicker.day(table, show, date) // TODO, when there is no table?
-    }
+  if (target) {
+    const from = currentTarget.value.indexOf('now') === -1 ? target.getAttribute(UUID) : Date.now()
+    const date = datepicker.parse(currentTarget.value, from)
+
+    return datepicker(target, date) // TODO, when there is no table?
   }
 })
 addEvent(UUID, 'keydown', (event) => {
@@ -111,20 +96,74 @@ addEvent(UUID, 'keydown', (event) => {
 })
 
 function onChange (event) {
-  const element = closest(event.target)
+  const {target} = closest(event.target)
+  console.log(event.type, element)
 
-  if (element) {
-    const content = element.nextElementSibling
+  if (target) {
+    const content = target.nextElementSibling
     const table = content.querySelector('table')
-    const date = new Date(element.getAttribute(UUID))
+    const date = new Date(target.getAttribute(UUID))
     const show = new Date(date)
 
     show.setFullYear(content.querySelector('[name="year"]').value)
     show.setMonth(content.querySelector('[name="month"]').value)
-    console.log(date, show)
 
     datepicker.day(table, show, date) // TODO, when there is no table?
   }
+}
+
+// Date.parse
+// + 1 second(s)
+// + 1 minute(s)
+// + 1 day(s)
+// + 1 month(s)
+// + 1 year(s)
+
+// - 1 minute(s)
+// - 1 day(s)
+// - 1 month(s)
+// - 1 year(s)
+
+// 00:00 - 1 year(s)
+// yyyy-mm-01 + 1 year
+// monday + 1 days
+
+// tue + 7 days(s)
+// friday = friday this week, can be in future and past
+// monday - 3 days = friday guaranteed last week
+// sunday + 5 days = friday guaranteed next week
+// 2018-01-dd + 1 week
+// yyyy-mm-01 + 1 month(s) - 1 day = end current month
+// yy00-01-01 - 100 year(s)
+
+datepicker.parse = (parse, now) => {
+  if (isFinite(new Date(parse))) return new Date(parse) // Try parsing natively first
+
+  const text = String(parse).toLowerCase()
+  const date = new Date(now || Date.now()) // Allow another now
+  const name = {year: 'FullYear', month: 'Month', day: 'Date', hour: 'Hours', minute: 'Minutes', second: 'Seconds'}
+  const math = /([+-]\s*\d+)\s*(second|minute|hour|day|month|year)|(mon)|(tue)|(wed)|(thu)|(fri)|(sat)|(sun)/g
+  const [, year = 'yyyy', month = 'mm', day = 'dd'] = text.match(/([\dy]{4})-([\dm]{2})-([\dd]{2})/) || []
+  const [, hour = 'hh', minute = 'mm', second = 'ss'] = text.match(/([\dh]{2}):([\dm]{2}):?([\ds]{2})?/) || []
+  let match = {year, month, day, hour, minute, second}
+
+  Object.keys(match).forEach((unit) => {
+    const move = unit === 'month' ? 1 : 0 // Fix zero based month index
+    const prev = `000${date[`get${name[unit]}`]() + move}`.slice(-match[unit].length) // PadLeft to same length
+    const next = match[unit].replace(/\D/g, (v, k) => prev[k]) // Replace non-digits with prev values
+    date[`set${name[unit]}`](next - move)
+  })
+
+  while((match = math.exec(text)) !== null) {
+    const unit = match[2]
+    const size = Number(String(match[1]).replace(/\s/g, '')) // Keep plus/minus but strip whitespace
+    const day = match.slice(2).indexOf(match[0]) // Weekdays starts at 3rd index but is not zero based
+
+    if (unit) date[`set${name[unit]}`](date[`get${name[unit]}`]() + size)
+    else date.setDate(date.getDate() - (date.getDay() || 7) + day) // Make sunday 7th day
+  }
+
+  return date
 }
 
 /*
--- packages/core-datepicker/core-datepicker.md
+++ packages/core-datepicker/core-datepicker.md
@@ -6,13 +6,19 @@ category: Components
 > `@nrk/core-datepicker` enhances a `<button>` or `<input>` field with keyboard accessible functionality for selecting date and time. The interface and granularity of date refinement can easily be altered through markup:
 
 ```datepicker.html
-<input type="text" name="">
-<button class="my-date" value="today">Choose date</button>  <!-- must be <button> -->
-<div class="my-dropdown">                                   <!-- hidden prevents flash of unstyled content -->
+<!-- <input type="text" name="">
+<button class="my-date">Choose date</button>  <!-- must be <button> or <input> -->
+<input type="text" class="my-date" placeholder="Choose date">
+<div class="my-dropdown">                     <!-- hidden prevents flash of unstyled content -->
   <div class="nrk-grid">
-    <div><button name="day" value="now">I dag</button></div>
-    <div><button name="day" value="-1 day">I går</button></div>
-    <div><button name="day" value="+1 day">I morgen</button></div>
+    <div><button data-core-datepicker value="now">I dag</button></div>
+    <div><button data-core-datepicker value="now - 1 day">I går</button></div>
+    <div><button data-core-datepicker value="now + 1 day">I morgen</button></div>
+    <div><button data-core-datepicker value="- 7 days">Tilbake en uke</button></div>
+    <div><button data-core-datepicker value="+ 7 days">Fremover en uke</button></div>
+    <div><button data-core-datepicker value="now tuesday - 7 days">Tirsdag sist uke</button></div>
+    <div><button data-core-datepicker value="now + 10 years">Om ti år</button></div>
+    <div><button data-core-datepicker value="yy00-01-01 - 100 years">Forrige århundre</button></div>
   </div>
   <label><span>Måned</span><select name="month"></select></label>
   <label><span>År</span><input type="number" name="year"></label>
@@ -21,8 +27,11 @@ category: Components
 ```
 ```datepicker.js
 coreDatepicker('.my-date')
-coreToggle('.my-date', {   // Make datepicker popup
-  open: true,
-  popup: true
-})
+// coreToggle('.my-date', {   // Make datepicker popup
+//   open: true,
+//   popup: true
+// })
+```
+```datepicker.css
+.my-dropdown button[aria-pressed="true"] { box-shadow: 0 0 0 1px }
 ```
