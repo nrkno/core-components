16c69e0878dd15f130b978244cc37adc13930cbf
Eirik Backer <Eirik.Backer@nrk.no>
2018-05-07 09:56:56 +0200

    Rename to core-date. Start refactor

--- packages/core-components.js
+++ packages/core-components.js
@@ -1,4 +1,4 @@
-export {default as datepicker} from '../packages/core-datepicker/core-datepicker.js'
+export {default as date} from '../packages/core-date/core-date.js'
 export {default as toggle} from '../packages/core-toggle/core-toggle.js'
 export {default as dialog} from '../packages/core-dialog/core-dialog.js'
 export {default as input} from '../packages/core-input/core-input.js'
