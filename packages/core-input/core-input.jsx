93a9cecbb4d629aa8e50426fcdb7d067eadc572e
Eirik Backer <Eirik.Backer@nrk.no>
2018-06-04 13:15:35 +0200

    Handle onToggle changes in JSX

--- packages/core-input/core-input.jsx
+++ packages/core-input/core-input.jsx
@@ -5,16 +5,23 @@ import {exclude} from '../utils'
 const DEFAULTS = {open: null, onFilter: null, onSelect: null}
 
 export default class Input extends React.Component {
+  constructor (props) {
+    super(props)
+    this.onFilter = this.onFilter.bind(this)
+    this.onSelect = this.onSelect.bind(this)
+  }
   componentDidMount () { // Mount client side only to avoid rerender
-    this.el.addEventListener('input.filter', this.props.onFilter)
-    this.el.addEventListener('input.select', this.props.onSelect)
+    this.el.addEventListener('input.filter', this.onFilter)
+    this.el.addEventListener('input.select', this.onSelect)
     coreInput(this.el.firstElementChild)
   }
   componentDidUpdate () { coreInput(this.el.firstElementChild) } // Must mount also on update in case content changes
   componentWillUnmount () {
-    this.el.removeEventListener('input.filter', this.props.onFilter)
-    this.el.removeEventListener('input.select', this.props.onSelect)
+    this.el.removeEventListener('input.filter', this.onFilter)
+    this.el.removeEventListener('input.select', this.onSelect)
   }
+  onFilter (event) { this.props.onFilter && this.props.onFilter(event) }
+  onSelect (event) { this.props.onSelect && this.props.onSelect(event) }
   render () {
     return React.createElement('div', exclude(this.props, DEFAULTS, {ref: el => (this.el = el)}),
       React.Children.map(this.props.children, (child, adjacent) => {
