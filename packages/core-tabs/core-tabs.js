e80a0e5d6d03949620f712c03ec3dbba955e131f
William Almnes <william.almnes@nrk.no>
2018-07-27 12:22:24 +0200

    always set tabindex=0 instead of removing it from inactive panels

--- packages/core-tabs/core-tabs.js
+++ packages/core-tabs/core-tabs.js
@@ -81,8 +81,8 @@ function setOpen (tablist, open) { // open can be Number, String or Element
     tab.setAttribute('aria-controls', panel.id = panel.id || getUUID())
     panel.setAttribute(`${ARIA}-labelledby`, tab.id = tab.id || getUUID())
     panel.setAttribute('role', 'tabpanel')
+    panel.setAttribute('tabindex', '0')
     panel[selectedPanel ? 'removeAttribute' : 'setAttribute']('hidden', '')
-    panel[selectedPanel ? 'setAttribute' : 'removeAttribute']('tabindex', '0')
   })
 
   // Setup after loop as we now know all tabs have IDs
