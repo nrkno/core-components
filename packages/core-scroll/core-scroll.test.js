a0771dffa44ca538ba9ecef7624f660cba0ca157
William Almnes <william.almnes@nrk.no>
2018-07-31 12:13:22 +0200

    add basic test for core-scroll

---
+++ packages/core-scroll/core-scroll.test.js
@@ -0,0 +1,27 @@
+const coreScroll = require('./core-scroll.min')
+const standardHTML = `
+<button data-core-scroll="my-scroll-js" value="up" aria-label="Rull opp">Up</button>
+<div>
+  <div id="my-scroll-js">
+    <div>Dette er en lang tekst</div>
+    <div>Dette er en lang tekst</div>
+    <div>Dette er en lang tekst</div>
+  </div>
+</div>
+`
+
+describe('core-scroll', () => {
+  it('should exist', () => {
+    expect(coreScroll).toBeInstanceOf(Function)
+  })
+
+  it('should initialize scroll container with appropriate styling', () => {
+    document.body.innerHTML = standardHTML
+
+    const scrollContainer = document.getElementById('my-scroll-js')
+    const expectedStyling = 'overflow: scroll; -webkit-overflow-scrolling: touch; max-height: calc(100% + 1px); margin-right: -1px; margin-bottom: -1px;'
+
+    coreScroll(scrollContainer)
+    expect(scrollContainer.getAttribute('style')).toEqual(expectedStyling)
+  })
+})
