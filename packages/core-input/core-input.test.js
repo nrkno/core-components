218e5b27af33e315a07c0e130ebb9f05b97acb07
William Almnes <william.almnes@nrk.no>
2018-07-31 14:18:16 +0200

    add some tests for core-input

---
+++ packages/core-input/core-input.test.js
@@ -0,0 +1,46 @@
+const coreInput = require('./core-input.min')
+
+const standardHTML = `
+<input type="text" class="my-input" placeholder="Type something...">
+<ul hidden>
+  <li><button>Chrome</button></li>
+  <li><button>Firefox</button></li>
+  <li><button>Opera</button></li>
+  <li><button>Safari</button></li>
+  <li><button>Microsoft Edge</button></li>
+</ul>
+`
+
+describe('core-input', () => {
+  it('should exists', () => {
+    expect(coreInput).toBeInstanceOf(Function)
+  })
+
+  it('should initialize input with props when core-input is called', () => {
+    document.body.innerHTML = standardHTML
+
+    const input = document.getElementsByClassName('my-input')[0]
+
+    coreInput(input)
+    expect(input.getAttribute('role')).toEqual('combobox')
+    expect(input.getAttribute('aria-autocomplete')).toEqual('list')
+    expect(input.getAttribute('autocomplete')).toEqual('off')
+    expect(input.getAttribute('aria-expanded')).toEqual('false')
+  })
+
+  it('should expand suggestions when input field is clicked', () => {
+    document.body.innerHTML = standardHTML
+
+    const input = document.getElementsByClassName('my-input')[0]
+    const suggestions = document.querySelector('.my-input + ul')
+
+    coreInput(input)
+
+    input.click()
+    expect(input.getAttribute('role')).toEqual('combobox')
+    expect(input.getAttribute('aria-autocomplete')).toEqual('list')
+    expect(input.getAttribute('autocomplete')).toEqual('off')
+    expect(input.getAttribute('aria-expanded')).toEqual('true')
+    expect(suggestions.hasAttribute('hidden')).toBeFalsy()
+  })
+})
