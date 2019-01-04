e3e493fc2462a87c6e30958bf418f7f90aaec03c
Eirik Backer <Eirik.Backer@nrk.no>
2019-01-04 13:49:57 +0100

    Fix tests

--- packages/core-toggle/core-toggle.js
+++ packages/core-toggle/core-toggle.js
@@ -13,7 +13,7 @@ export default function toggle (toggles, open) {
   return queryAll(toggles).map((toggle) => {
     const isOpen = toggle.getAttribute(OPEN) === 'true'
     const open = typeof options.open === 'boolean' ? options.open : (options.open === 'toggle' ? !isOpen : isOpen)
-    const popup = options.hasOwnProperty('popup') ? String(options.popup || false) : toggle.getAttribute(UUID)
+    const popup = String((options.hasOwnProperty('popup') ? options.popup : toggle.getAttribute(UUID)) || false)
     const content = getContentElement(toggle)
 
     toggle.setAttribute(UUID, popup) // aria-haspopup triggers forms mode in JAWS, therefore store in uuid
