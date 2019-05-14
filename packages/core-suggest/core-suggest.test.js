07954a3f8a687d7f8fa80a8a1840791eb184107e
Herman Torjussen <herman.torjussen@nrk.no>
2019-05-14 14:15:02 +0200

    convert tests for core-suggest

--- packages/core-suggest/core-suggest.test.js
+++ packages/core-suggest/core-suggest.test.js
@@ -1,161 +1,120 @@
-const HTML = `
-  <input type="text" placeholder="Type something...">
-  <ul hidden>
-    <li><button>Chrome</button></li>
-    <li><button>Firefox</button></li>
-    <li><button>Opera</button></li>
-    <li><button>Safari</button></li>
-    <li><button>Microsoft Edge</button></li>
-  </ul>
-  <button></button>
-`
+import test from 'ava'
+import path from 'path'
+import puppeteer from 'puppeteer'
 
-describe.skip('core-input', () => {
-  beforeEach(() => {
-    jest.spyOn(window, 'requestAnimationFrame').mockImplementation(cb => cb())
-  })
+async function withPage (t, run) {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  page.on('console', msg => console.log(msg._text))
+  await page.addScriptTag({ path: path.join(__dirname, 'core-suggest.min.js') })
+  try {
+    await run(t, page)
+  } finally {
+    await page.close()
+    await browser.close()
+  }
+}
 
-  afterEach(() => {
-    window.requestAnimationFrame.mockRestore()
-  })
+test('sets up properties', withPage, async (t, page) => {
+  await page.setContent(`<input type="text"><core-suggest hidden></core-suggest>`)
+  t.is(await page.$eval('input', el => el.getAttribute('aria-autocomplete')), 'list')
+  t.is(await page.$eval('input', el => el.getAttribute('autocomplete')), 'off')
+  t.is(await page.$eval('input', el => el.getAttribute('aria-expanded')), 'false')
+})
 
-  it('should exists', () => {
-    expect(coreInput).toBeInstanceOf(Function)
-  })
+test('opens suggestions on input focus', withPage, async (t, page) => {
+  await page.setContent(`<input type="text"><core-suggest hidden></core-suggest>`)
+  await page.click('input')
+  t.is(await page.$eval('input', el => el.getAttribute('aria-expanded')), 'true')
+  t.false(await page.$eval('core-suggest', el => el.hasAttribute('hidden')))
+})
 
-  it('should initialize input with props', () => {
-    document.body.innerHTML = HTML
-    const input = document.querySelector('input')
-    coreInput(input)
-    expect(input.getAttribute('role')).toEqual('combobox')
-    expect(input.getAttribute('aria-autocomplete')).toEqual('list')
-    expect(input.getAttribute('autocomplete')).toEqual('off')
-    expect(input.getAttribute('aria-expanded')).toEqual('false')
-  })
+test('closes suggestions on click outside', withPage, async (t, page) => {
+  await page.setContent(`<input type="text"><core-suggest hidden></core-suggest>`)
+  await page.click('input')
+  t.false(await page.$eval('core-suggest', el => el.hasAttribute('hidden')))
+  await page.click('body')
+  t.true(await page.$eval('core-suggest', el => el.hasAttribute('hidden')))
+})
 
-  it('should expand suggestions when input is clicked', () => {
-    document.body.innerHTML = HTML
-    const input = document.querySelector('input')
-    const suggestions = document.querySelector('input + ul')
-    coreInput(input)
-    input.click()
-    expect(input.getAttribute('role')).toEqual('combobox')
-    expect(input.getAttribute('aria-autocomplete')).toEqual('list')
-    expect(input.getAttribute('autocomplete')).toEqual('off')
-    expect(input.getAttribute('aria-expanded')).toEqual('true')
-    expect(suggestions.hasAttribute('hidden')).toBeFalsy()
-  })
+test('sets input value to selected suggestion', withPage, async (t, page) => {
+  await page.setContent(`
+    <input type="text">
+    <core-suggest hidden>
+      <ul>
+        <li><button>Suggest 1</button>
+        <li><button>Suggest 2</button>
+      </ul>
+    </core-suggest>
+  `)
+  await page.click('input')
+  await page.click('button')
+  t.is(await page.$eval('input', el => el.value), 'Suggest 1')
+})
 
-  it('should set input value to clicked suggestion', () => {
-    document.body.innerHTML = HTML
-    const input = document.querySelector('input')
-    const suggestions = document.querySelector('input + ul')
-    const firefoxBtn = suggestions.querySelector('li:nth-child(2) button')
-    const callback = jest.fn()
-    coreInput(input)
-    input.addEventListener('input.select', callback)
-    input.click()
-    firefoxBtn.click()
-    expect(callback).toHaveBeenCalled()
-    expect(input.value).toEqual('Firefox')
-    expect(input.getAttribute('role')).toEqual('combobox')
-    expect(input.getAttribute('aria-autocomplete')).toEqual('list')
-    expect(input.getAttribute('autocomplete')).toEqual('off')
-    expect(input.getAttribute('aria-expanded')).toEqual('false')
-    expect(suggestions.hasAttribute('hidden')).toBeTruthy()
-  })
+test('filters suggestions from input value', withPage, async (t, page) => {
+  await page.setContent(`
+    <input type="text">
+    <core-suggest hidden>
+      <ul>
+        <li><button>Suggest 1</button>
+        <li><button>Suggest 2</button>
+        <li><button>Suggest 3</button>
+      </ul>
+    </core-suggest>
+  `)
+  await page.type('input', '2')
+  t.true(await page.$eval('li:nth-child(1) button', el => el.hasAttribute('hidden')))
+  t.false(await page.$eval('li:nth-child(2) button', el => el.hasAttribute('hidden')))
+  t.true(await page.$eval('li:nth-child(3) button', el => el.hasAttribute('hidden')))
+})
 
-  it('should close suggestions on focusing outside', () => {
-    document.body.innerHTML = HTML
-    const input = document.querySelector('input')
-    const suggestions = document.querySelector('input + ul')
-    const button = document.querySelector('button')
-    coreInput(input)
-    input.click()
-    button.click()
-    expect(input.getAttribute('role')).toEqual('combobox')
-    expect(input.getAttribute('aria-autocomplete')).toEqual('list')
-    expect(input.getAttribute('autocomplete')).toEqual('off')
-    expect(input.getAttribute('aria-expanded')).toEqual('false')
-    expect(suggestions.hasAttribute('hidden')).toBeTruthy()
-  })
+test('sets type="button" on all suggestion buttons', withPage, async (t, page) => {
+  await page.setContent(`
+    <input type="text">
+    <core-suggest hidden>
+      <ul>
+        <li><button>Suggest 1</button>
+        <li><button>Suggest 2</button>
+        <li><button>Suggest 3</button>
+      </ul>
+    </core-suggest>
+  `)
+  t.true(await page.$$eval('button', els => els.every(el => el.getAttribute('type') === 'button')))
+})
 
-  it('should filter suggestion from input value', () => {
-    document.body.innerHTML = HTML
-    const input = document.querySelector('input')
-    const event = new window.CustomEvent('input', { bubbles: true })
-    coreInput(input)
-    input.value = 'Chrome'
-    input.dispatchEvent(event)
-    expect(document.querySelectorAll('button[hidden]').length).toEqual(4)
-  })
+test('sets up and parses limit option', withPage, async (t, page) => {
+  await page.setContent(`<input type="text"><core-suggest hidden></core-suggest>`)
+  t.is(await page.$eval('core-suggest', el => el.limit), Infinity)
+  await page.$eval('core-suggest', el => el.limit = 2)
+  t.is(await page.$eval('core-suggest', el => el.limit), 2)
+  await page.$eval('core-suggest', el => el.limit = -2)
+  t.is(await page.$eval('core-suggest', el => el.limit), 0)
+  await page.$eval('core-suggest', el => el.limit = null)
+  t.is(await page.$eval('core-suggest', el => el.limit), 0)
+  await page.$eval('core-suggest', el => el.limit = undefined)
+  t.is(await page.$eval('core-suggest', el => el.limit), 0)
+})
 
-  it('should set type="button" on all buttons in list', () => {
-    document.body.innerHTML = HTML
-    coreInput(document.querySelector('input'))
-    document.querySelectorAll('ul button').forEach((button) => {
-      expect(button.type).toEqual('button')
-    })
-  })
-
-  it('should remember and ovewrite options', () => {
-    document.body.innerHTML = HTML
-    const input = document.querySelector('input')
-    coreInput(input, { limit: 11, ajax: 'https://example.com/{{value}}' })
-    expect(input.getAttribute(`${UUID}-limit`)).toEqual('11')
-    coreInput(input, { limit: 12 })
-    coreInput(input)
-    expect(input.getAttribute(`${UUID}-limit`)).toEqual('12')
-    expect(input.getAttribute(UUID)).toEqual('https://example.com/{{value}}')
-  })
-
-  it('should correctly parse limit option', () => {
-    document.body.innerHTML = HTML
-    const input = document.querySelector('input')
-    coreInput(input)
-    expect(input.getAttribute(`${UUID}-limit`)).toBe('0')
-    coreInput(input, { limit: 2 })
-    expect(input.getAttribute(`${UUID}-limit`)).toBe('2')
-    coreInput(input, { limit: 0 })
-    expect(input.getAttribute(`${UUID}-limit`)).toBe('0')
-    coreInput(input, { limit: -2 })
-    expect(input.getAttribute(`${UUID}-limit`)).toBe('0')
-    coreInput(input, { limit: null })
-    expect(input.getAttribute(`${UUID}-limit`)).toBe('0')
-    coreInput(input, { limit: undefined })
-    expect(input.getAttribute(`${UUID}-limit`)).toBe('0')
-    coreInput(input, { limit: 2 })
-    coreInput(input, { limit: undefined })
-    expect(input.getAttribute(`${UUID}-limit`)).toBe('2')
-  })
-
-  it('should limit length of suggestions from limit option', () => {
-    document.body.innerHTML = HTML
-    const input = document.querySelector('input')
-    const suggestions = document.querySelector('input + ul')
-    coreInput(input)
-    expect(suggestions.children[0].hasAttribute('hidden')).toBe(false)
-    expect(suggestions.children[1].hasAttribute('hidden')).toBe(false)
-    expect(suggestions.children[2].hasAttribute('hidden')).toBe(false)
-    expect(suggestions.children[3].hasAttribute('hidden')).toBe(false)
-    expect(suggestions.children[4].hasAttribute('hidden')).toBe(false)
-    coreInput(input, { limit: 3 })
-    expect(suggestions.children[0].hasAttribute('hidden')).toBe(false)
-    expect(suggestions.children[1].hasAttribute('hidden')).toBe(false)
-    expect(suggestions.children[2].hasAttribute('hidden')).toBe(false)
-    expect(suggestions.children[3].hasAttribute('hidden')).toBe(true)
-    expect(suggestions.children[4].hasAttribute('hidden')).toBe(true)
-    coreInput(input, { limit: undefined })
-    expect(suggestions.children[0].hasAttribute('hidden')).toBe(false)
-    expect(suggestions.children[1].hasAttribute('hidden')).toBe(false)
-    expect(suggestions.children[2].hasAttribute('hidden')).toBe(false)
-    expect(suggestions.children[3].hasAttribute('hidden')).toBe(true)
-    expect(suggestions.children[4].hasAttribute('hidden')).toBe(true)
-    coreInput(input, { limit: 1 })
-    expect(suggestions.children[0].hasAttribute('hidden')).toBe(false)
-    expect(suggestions.children[1].hasAttribute('hidden')).toBe(true)
-    expect(suggestions.children[2].hasAttribute('hidden')).toBe(true)
-    expect(suggestions.children[3].hasAttribute('hidden')).toBe(true)
-    expect(suggestions.children[4].hasAttribute('hidden')).toBe(true)
-  })
+test('limits suggestions from limit option', withPage, async (t, page) => {
+  await page.setContent(`
+    <input type="text">
+    <core-suggest limit="2" hidden>
+      <ul>
+        <li><button>Suggest 1</button>
+        <li><button>Suggest 2</button>
+        <li><button>Suggest 3</button>
+        <li><button>Suggest 4</button>
+      </ul>
+    </core-suggest>
+  `)
+  t.false(await page.$eval('li:nth-child(1) button', el => el.hasAttribute('hidden')))
+  t.false(await page.$eval('li:nth-child(2) button', el => el.hasAttribute('hidden')))
+  t.true(await page.$eval('li:nth-child(3) button', el => el.hasAttribute('hidden')))
+  t.true(await page.$eval('li:nth-child(4) button', el => el.hasAttribute('hidden')))
+  await page.$eval('core-suggest', el => el.limit = 3)
+  t.false(await page.$eval('li:nth-child(1) button', el => el.hasAttribute('hidden')))
+  t.false(await page.$eval('li:nth-child(2) button', el => el.hasAttribute('hidden')))
+  t.false(await page.$eval('li:nth-child(3) button', el => el.hasAttribute('hidden')))
+  t.true(await page.$eval('li:nth-child(4) button', el => el.hasAttribute('hidden')))
 })
