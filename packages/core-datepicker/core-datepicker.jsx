82a4ab75ab8df4e06ff51448146fac7566a2a28b
Herman Torjussen <herman.torjussen@nrk.no>
2019-05-27 16:43:26 +0200

    fix datepicker props; update custom2react (again)

--- packages/core-datepicker/core-datepicker.jsx
+++ packages/core-datepicker/core-datepicker.jsx
@@ -2,5 +2,6 @@ import CoreDatepicker from './core-datepicker.js'
 import customElementToReact from '@nrk/custom-element-to-react'
 
 export default customElementToReact(CoreDatepicker, {
-  customEvents: ['datepicker.change', 'datepicker.click.day', 'disabled', 'months', 'days']
+  props: ['disabled', 'months', 'days'],
+  customEvents: ['datepicker.change', 'datepicker.click.day']
 })
