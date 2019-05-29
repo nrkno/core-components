e2f44be1b142209c724e30e111031e5ae7b10426
Herman Torjussen <herman.torjussen@nrk.no>
2019-05-29 09:58:49 +0200

    add version number in suffix (#262)

--- packages/core-tabs/core-tabs.jsx
+++ packages/core-tabs/core-tabs.jsx
@@ -1,7 +1,9 @@
 import CoreTabs from './core-tabs.js'
+import { version } from './package.json'
 import customElementToReact from '@nrk/custom-element-to-react'
 
 export default customElementToReact(CoreTabs, {
   props: ['tab'],
-  customEvents: ['tabs.toggle']
+  customEvents: ['tabs.toggle'],
+  suffix: version
 })
--- packages/core-toggle/core-toggle.jsx
+++ packages/core-toggle/core-toggle.jsx
@@ -1,6 +1,8 @@
 import CoreToggle from './core-toggle.js'
+import { version } from './package.json'
 import customElementToReact from '@nrk/custom-element-to-react'
 
 export default customElementToReact(CoreToggle, {
-  customEvents: ['toggle', 'toggle.select']
+  customEvents: ['toggle', 'toggle.select'],
+  suffix: version
 })
