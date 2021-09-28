86faaab9ffa8f0d850eae6b80ba5ef458a034d9b
Skjalg Teig <skjalgepalg@gmail.com>
2021-09-28 11:00:25 +0200

    core-toggle: Remove prop definition of popup for React/JSX

--- packages/core-toggle/core-toggle.jsx
+++ packages/core-toggle/core-toggle.jsx
@@ -3,7 +3,6 @@ import { version } from './package.json'
 import customElementToReact from '@nrk/custom-element-to-react'
 
 export default customElementToReact(CoreToggle, {
-  props: ['popup'],
   customEvents: ['toggle', 'toggle.select'],
   suffix: version
 })
