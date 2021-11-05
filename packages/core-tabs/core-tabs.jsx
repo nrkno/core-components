b7beb843bd2dd823ba231c045f11fbf9252d89f5
Skjalg Teig <skjalgepalg@gmail.com>
2021-11-05 16:09:29 +0100

    feat: Use tab as attribute, not prop for React

--- packages/core-tabs/core-tabs.jsx
+++ packages/core-tabs/core-tabs.jsx
@@ -3,7 +3,6 @@ import { version } from './package.json'
 import customElementToReact from '@nrk/custom-element-to-react'
 
 export default customElementToReact(CoreTabs, {
-  props: ['tab'],
   customEvents: ['tabs.toggle'],
   suffix: version
 })
