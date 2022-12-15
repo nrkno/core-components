8050f021b2085a9c0f4ce5e734c2f4133830a173
William Almnes <william.almnes@gmail.com>
2022-12-15 11:16:58 +0100

    ensure core toggle react element has same funcitonality as vanilla when value changes

--- packages/core-toggle/core-toggle.jsx
+++ packages/core-toggle/core-toggle.jsx
@@ -4,5 +4,6 @@ import customElementToReact from '@nrk/custom-element-to-react'
 
 export default customElementToReact(CoreToggle, {
   customEvents: ['toggle', 'toggle.select'],
+  props: ['value'],
   suffix: version
 })
