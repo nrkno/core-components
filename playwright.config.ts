636981e21c90583519b01f3033fc4e0cf848b93c
Ole Christer Selvig <ole.christer.andre.asikainen.selvig@nrk.no>
2023-07-16 18:52:07 +0200

    ci(playwright): run *.spec.ts tests only

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
-
+  testMatch: "*.spec.ts",
   /* Configure projects for major browsers */
   projects: [
     {
