d3ef7eae67f3587f07967bbf1857dd35d9a79ddc
Ole Christer Selvig <ole.christer.andre.asikainen.selvig@nrk.no>
2023-07-19 12:01:07 +0200

    fix: redudant use property

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: "http://127.0.0.1:10003",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -73,8 +73,5 @@ export default defineConfig({
     command: 'npm run test-server',
     url: 'http://127.0.0.1:10003',
     reuseExistingServer: !process.env.CI,
-  }, 
-  use: {
-    baseURL: "http://127.0.0.1:10003"
-  }
+  },
 });
