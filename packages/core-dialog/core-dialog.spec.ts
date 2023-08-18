95116b1206c6205c96132c6e57f04a15b43913ca
Ole Christer Selvig <ole.christer.andre.asikainen.selvig@nrk.no>
2023-08-18 14:12:09 +0200

    fix(core-toggle-test): use setContent

--- packages/core-dialog/core-dialog.spec.ts
+++ packages/core-dialog/core-dialog.spec.ts
@@ -1,44 +1,66 @@
 import { test, expect, Locator } from '@playwright/test';
 
-const setTemplate = ({ templateId, containerId }) => {
-  const template: HTMLTemplateElement | null = document.querySelector(`[data-testid="${templateId}"]`)
-  if (template) {
-    const container: HTMLElement | null = document.querySelector(`[data-testid="${containerId}"]`)
-    if (container) {
-      container.appendChild(template.content.cloneNode(true))
-    } else {
-      throw Error(`html custom element with data-testid: ${containerId} not found`)
-    }
-  } else {
-    throw Error(`html template element with data-testid: ${templateId} not found`)
+const defaultStyle = `
+  core-dialog:not([hidden]) {
+    display: block;
+    position: relative;
   }
-}
+
+  body {
+    height: 100vh;
+    width: 100vw;
+  }
+
+  backdrop:not([hidden]), .my-backdrop:not([hidden]) {
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, .1);
+  }
+`
 
 test.describe('core-dialog', () => {
   let coreDialog: Locator;
 
   test.beforeEach(async ({ page }) => {
     await page.goto('./core-dialog/core-dialog.spec.html')
-    coreDialog = await page.getByTestId('core-dialog')
+    coreDialog = page.getByTestId('core-dialog')
   })
   
   test('sets up properties', async ({ page }) => {
-    await page.evaluate(setTemplate, ({ containerId: 'core-dialog-container', templateId: 'default-template' }))
+    await page.setContent(`
+      <button data-for="core-dialog">Open</button>
+      <core-dialog data-testid="core-dialog" id="core-dialog" hidden>
+        <div>Some content</div>
+        <button data-for="close">Close</button>
+      </core-dialog>
+    `)
+    await page.addStyleTag({ content: defaultStyle })
     await expect(coreDialog).toBeHidden()
     await expect(coreDialog).toHaveAttribute('role', 'dialog')
     await expect(coreDialog).toHaveAttribute('aria-modal', 'true')
   })
   
   test('opens and closes', async ({ page }) => {
-    await page.evaluate(setTemplate, ({ containerId: 'core-dialog-container', templateId: 'default-template' }))
+    await page.setContent(`
+      <button data-for="core-dialog">Open</button>
+      <core-dialog data-testid="core-dialog" id="core-dialog" hidden>
+        <div>Some content</div>
+        <button data-for="close">Close</button>
+      </core-dialog>
+    `)
+    await page.addStyleTag({ content: defaultStyle })
     
     // click event
     
-    await page.getByRole('button').and(page.getByText('Open', { exact: true })).click()
+    await page.getByRole('button').and(page.getByText('Open')).click()
     await expect(coreDialog).toBeVisible()
     await expect(page.locator('backdrop')).toBeVisible()
 
-    await page.getByRole('button').and(page.getByText('Close', { exact: true })).click()
+    await page.getByRole('button').and(page.getByText('Close')).click()
     await expect(coreDialog).toBeHidden()
     await expect(page.locator('backdrop')).toBeHidden()
 
@@ -54,21 +76,41 @@ test.describe('core-dialog', () => {
   })
   
   test('respects deprecated "for" attribute', async ({ page }) => {
-    await page.evaluate(setTemplate, ({ containerId: 'core-dialog-container', templateId: 'default-template' }))
+    await page.setContent(`
+      <button data-for="core-dialog">Open</button>
+      <core-dialog data-testid="core-dialog" id="core-dialog" hidden>
+        <div>Some content</div>
+        <button for="close">Close</button>
+      </core-dialog>
+    `)
+    await page.addStyleTag({ content: defaultStyle })
     
-    await page.getByRole('button').and(page.getByText('Deprecated Open')).click()
+    await page.getByRole('button').and(page.getByText('Open')).click()
     await expect(coreDialog).toBeVisible()
     await expect(page.locator('backdrop')).toBeVisible()
 
-    await page.getByRole('button').and(page.getByText('Deprecated Close')).click()
+    await page.getByRole('button').and(page.getByText('Close')).click()
     await expect(coreDialog).toBeHidden()
     await expect(page.locator('backdrop')).toBeHidden()
   })
   
   test('opens and closes nested', async ({ page }) => {
-    await page.evaluate(setTemplate, ({ containerId: 'core-dialog-container', templateId: 'nested-dialog-template' }))
+    await page.setContent(`
+      <button data-for="core-dialog">Open</button>
+      <core-dialog data-testid="core-dialog" id="core-dialog" hidden>
+        <div>Some content</div>
+        <button type="button" autofocus>Autofocus</button>
+        <button data-for="nested-core-dialog">Open nested</button>
+        <core-dialog id="nested-core-dialog" hidden>
+          <div>Nested content</div>
+          <button data-for="close">Close nested</button>
+        </core-dialog>
+        <button data-for="close">Close</button>
+      </core-dialog>
+    `)
+    await page.addStyleTag({ content: defaultStyle })
 
-    await page.getByRole('button').and(page.getByText('Open', { exact: true })).click()
+    await page.getByRole('button').and(page.getByText('Open')).click()
     await expect(coreDialog.getByText('Some content')).toBeVisible()
     await coreDialog.getByRole('button').and(coreDialog.getByText('Open nested')).click()
 
@@ -84,9 +126,22 @@ test.describe('core-dialog', () => {
   })
   
   test('loses nested with pressed esc', async ({ page }) => {
-    await page.evaluate(setTemplate, ({ containerId: 'core-dialog-container', templateId: 'nested-dialog-template' }))
+    await page.setContent(`
+      <button data-for="core-dialog">Open</button>
+      <core-dialog data-testid="core-dialog" id="core-dialog" hidden>
+        <div>Some content</div>
+        <button type="button" autofocus>Autofocus</button>
+        <button data-for="nested-core-dialog">Open nested</button>
+        <core-dialog id="nested-core-dialog" hidden>
+          <div>Nested content</div>
+          <button data-for="close">Close nested</button>
+        </core-dialog>
+        <button data-for="close">Close</button>
+      </core-dialog>
+    `)
+    await page.addStyleTag({ content: defaultStyle })
     
-    await page.getByRole('button').and(page.getByText('Open', { exact: true })).click()
+    await page.getByRole('button').and(page.getByText('Open')).click()
     await expect(coreDialog.getByText('Some content')).toBeVisible()
     await coreDialog.getByRole('button').and(coreDialog.getByText('Open nested')).click()
     await expect(coreDialog.getByText('Nested content')).toBeVisible()
@@ -100,33 +155,71 @@ test.describe('core-dialog', () => {
   })
   
   test('defaults to backdrop="on" when not supplied', async ({ page }) => {
-    await page.evaluate(setTemplate, ({ containerId: 'core-dialog-container', templateId: 'default-template' }))
-    await page.getByRole('button').and(page.getByText('Open', { exact: true })).click()
+    await page.setContent(`
+      <button data-for="core-dialog">Open</button>
+      <core-dialog data-testid="core-dialog" id="core-dialog" hidden>
+        <div>Some content</div>
+        <button data-for="close">Close</button>
+      </core-dialog>
+    `)
+    await page.addStyleTag({ content: defaultStyle })
+    await page.getByRole('button').and(page.getByText('Open')).click()
     await expect(coreDialog).toHaveJSProperty('hidden', false)
     await expect(page.locator('backdrop')).toBeAttached()
   })
   
   test('respects explicit backdrop="on"', async ({ page }) => {
-    await page.evaluate(setTemplate, ({ containerId: 'core-dialog-container', templateId: 'backdrop-on-template' }))
+    await page.setContent(`
+      <button data-for="core-dialog">Open</button>
+      <core-dialog data-testid="core-dialog" id="core-dialog" backdrop="on" hidden>
+        <div>Some content</div>
+        <button data-for="close">Close</button>
+        <button for="close">Deprecated Close</button>
+      </core-dialog>¯
+    `)
+    await page.addStyleTag({ content: defaultStyle })
     await expect(coreDialog).toBeHidden()
     await expect(page.locator('backdrop')).toBeAttached()
   })
   
   test('respects backdrop with no value, defaulting to "on"', async ({ page }) => {
-    await page.evaluate(setTemplate, ({ containerId: 'core-dialog-container', templateId: 'backdrop-template' }))
+    await page.setContent(`
+      <button data-for="core-dialog">Open</button>
+      <core-dialog data-testid="core-dialog" id="core-dialog" backdrop hidden>
+        <div>Some content</div>
+        <button data-for="close">Close</button>
+        <button for="close">Deprecated Close</button>
+      </core-dialog>
+    `)
+    await page.addStyleTag({ content: defaultStyle })
     await expect(coreDialog).toBeHidden()
     await expect(page.locator('backdrop')).toBeAttached()
   })
   
   test('respects backdrop="off"', async ({ page }) => {
-    await page.evaluate(setTemplate, ({ containerId: 'core-dialog-container', templateId: 'backdrop-off-template' }))
+    await page.setContent(`
+      <button data-for="core-dialog">Open</button>
+      <core-dialog data-testid="core-dialog" id="core-dialog" backdrop="off" hidden>
+        <div>Some content</div>
+        <button data-for="close">Close</button>
+        <button for="close">Deprecated Close</button>
+      </core-dialog>
+    `)
+    await page.addStyleTag({ content: defaultStyle })
     await expect(coreDialog).toBeHidden()
     await expect(page.locator('backdrop')).not.toBeAttached()
   })
   
   test('respects backdrop with custom id', async ({ page }) => {
-    await page.evaluate(setTemplate, ({ containerId: 'core-dialog-container', templateId: 'backdrop-custom-template' }))
-    await page.getByRole('button').and(page.getByText('Open', { exact: true })).click()
+    await page.setContent(`
+      <button data-for="core-dialog">Open</button>
+      <core-dialog data-testid="core-dialog" id="core-dialog" backdrop="custom-backdrop" hidden>
+        <button data-for="close">Close</button>
+      </core-dialog>
+      <div data-testid="custom-backdrop" id="custom-backdrop" class="my-backdrop" hidden></div>
+    `)
+    await page.addStyleTag({ content: defaultStyle })
+    await page.getByRole('button').and(page.getByText('Open')).click()
     await expect(coreDialog).toBeVisible()
     await expect(page.locator('backdrop')).not.toBeAttached()
     await expect(page.getByTestId('custom-backdrop')).toBeVisible()
@@ -135,8 +228,15 @@ test.describe('core-dialog', () => {
   test('displays no backdrop and logs warning when custom backdrop is provided and not found', async ({ page }) => {
     let consoleMsg
     page.once('console', msg => consoleMsg = msg.text())
-    await page.evaluate(setTemplate, ({ containerId: 'core-dialog-container', templateId: 'backdrop-custom-invalid-template' }))
-    await page.getByRole('button').and(page.getByText('Open', { exact: true })).click()
+    await page.setContent(`
+      <button data-for="core-dialog">Open</button>
+      <core-dialog data-testid="core-dialog" id="core-dialog" backdrop="invalid-reference-id" hidden>
+        <button data-for="close">Close</button>
+      </core-dialog>
+      <div data-testid="custom-backdrop" id="custom-backdrop" class="my-backdrop" hidden></div>
+    `)
+    await page.addStyleTag({ content: defaultStyle })
+    await page.getByRole('button').and(page.getByText('Open')).click()
     await expect(coreDialog).toBeVisible()
     await expect(page.getByTestId('custom-backdrop')).toBeHidden()
     await expect(page.locator('backdrop')).not.toBeAttached()
@@ -144,18 +244,39 @@ test.describe('core-dialog', () => {
   })
   
   test('respects strict option', async ({ page }) => {
-    await page.evaluate(setTemplate, ({ containerId: 'core-dialog-container', templateId: 'strict-template' }))
-    await page.getByRole('button').and(page.getByText('Open', { exact: true })).click()
+    await page.setContent(`
+      <button data-for="core-dialog">Open</button>
+      <core-dialog data-testid="core-dialog" id="core-dialog" strict hidden>
+        <button data-for="close">Close</button>
+      </core-dialog>
+    `)
+    await page.addStyleTag({ content: defaultStyle })
+    await page.getByRole('button').and(page.getByText('Open')).click()
     await expect(coreDialog).toBeVisible()
     await page.locator('backdrop').click()
     await expect(coreDialog).toBeVisible()
   })
   
   test('triggers toggle event', async ({ page }) => {
-    await page.evaluate(setTemplate, ({ containerId: 'core-dialog-container', templateId: 'default-template' }))
-    await page.getByRole('button').and(page.getByText('Open', { exact: true })).click()
-    await expect(page.getByTestId('num-toggle-events')).toHaveText('1')
-    await page.getByRole('button').and(page.getByText('Close', { exact: true })).click()
-    await expect(page.getByTestId('num-toggle-events')).toHaveText('2')
+    let numToggleEvents = 0
+    await page.exposeFunction('captureToggleEvent', () => numToggleEvents += 1)
+    await page.setContent(`
+      <button data-for="core-dialog">Open</button>
+      <core-dialog data-testid="core-dialog" id="core-dialog" hidden>
+        <div>Some content</div>
+        <button data-for="close">Close</button>
+      </core-dialog>
+    `)
+    await page.addScriptTag({ content: `
+      document.addEventListener('dialog.toggle', window.captureToggleEvent)
+    `})
+    await page.addStyleTag({ content: defaultStyle })
+    expect(numToggleEvents).toBe(0)
+    await page.getByRole('button').and(page.getByText('Open')).click()
+    await page.waitForFunction('window.captureToggleEvent')
+    expect(numToggleEvents).toBe(1)
+    await page.getByRole('button').and(page.getByText('Close')).click()
+    await page.waitForFunction('window.captureToggleEvent')
+    expect(numToggleEvents).toBe(2)
   })
 })
