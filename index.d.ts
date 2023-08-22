64ec174f836344b508a546caef856c245aa999fc
Ole Christer Selvig <ole.christer.andre.asikainen.selvig@nrk.no>
2023-08-22 15:09:34 +0200

    fix: use tripple-slash reference in index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,10 +2,10 @@
  * Typescript type definitions are currently only available in development (un-published)
  */
 
-import CoreDatepicker from './packages/core-datepicker/core-datepicker'
-import CoreDialog from './packages/core-dialog/core-dialog'
-import CoreProgress from './packages/core-progress/core-progress'
-import CoreScroll from './packages/core-scroll/core-scroll'
-import CoreSuggest from './packages/core-suggest/core-suggest'
-import CoreTabs from './packages/core-tabs/core-tabs'
-import CoreToggle from './packages/core-toggle/core-toggle'
+/// <reference path="./packages/core-datepicker/core-datepicker.d.ts" />
+/// <reference path="./packages/core-dialog/core-dialog.d.ts" />
+/// <reference path="./packages/core-progress/core-progress.d.ts" />
+/// <reference path="./packages/core-scroll/core-scroll.d.ts" />
+/// <reference path="./packages/core-suggest/core-suggest.d.ts" />
+/// <reference path="./packages/core-tabs/core-tabs.d.ts" />
+/// <reference path="./packages/core-toggle/core-toggle.d.ts" />
